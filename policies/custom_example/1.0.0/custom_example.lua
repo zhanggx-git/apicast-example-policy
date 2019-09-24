83949abb184780a24d42ef266e22e5fe901101de
zhanggx-git <zhanggx-git@xxx.com>
2019-09-24 16:10:39 +0800

    custom example

--- policies/custom_example/1.0.0/custom_example.lua
+++ policies/custom_example/1.0.0/custom_example.lua
@@ -1,11 +1,30 @@
-local setmetatable = setmetatable
+local _M = require('apicast.policy').new('Custom Example', '1.0.0')
 
-local _M = require('apicast.policy').new('Example', '0.1')
-local mt = { __index = _M }
+local new = _M.new
 
-function _M.new()
-  return setmetatable({}, mt)
+local ipairs = ipairs
+local insert = table.insert
+
+function _M.new(configuration)
+  local self = new()
+
+  local ops = {}
+
+  local config = configuration or {}
+  local set_header = config.set_header or {}
+
+  for _, header in ipairs(set_header) do
+    insert(ops, function()
+      ngx.log(ngx.NOTICE, 'setting header: ', header.name, ' to: ', header.value)
+      ngx.req.set_header(header.name, header.value)
+    end)
+  end
+
+  self.ops = ops
+
+  return self
 end
+
 
 function _M:init()
   -- do work when nginx master process starts
@@ -17,6 +36,9 @@ end
 
 function _M:rewrite()
   -- change the request before it reaches upstream
+  for _,op in ipairs(self.ops) do
+    op()
+  end
 end
 
 function _M:access()
